30cbf1cdae77b856a00dd240dcf452e8eadecd1e
Cezary Olszewski <czarekolszewski99@gmail.com>
2023-09-14 21:53:55 +0200

    Add new class Numbers with methods scanA and scanB

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,35 @@
 import java.util.Scanner;
 
+class Numbers {
+    int a;
+    int b;
+    public void scanA() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Write first number for calculations:");
+        a = sc.nextInt();
+    }public void scanB() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Write second number for calculations:");
+        b = sc.nextInt();
+    }
+}
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Write first number to calculations:");
-        int a = sc.nextInt();
-        System.out.println("Write second number to calculations:");
-        int b = sc.nextInt();
+        Numbers num = new Numbers();
+        num.scanA();
+        num.scanB();
 
         //Addition
-        int sum = a + b;
-        System.out.println("Result of sum numbers " + a + " and " + b + ": " + sum);
+        int sum = num.a + num.b;
+        System.out.println("Result of sum numbers " + num.a + " and " + num.b + ": " + sum);
         //Difference
-        int diff = a - b;
-        System.out.println("Result of difference numbers " + a + " and " + b + ": " + diff);
+        int diff = num.a - num.b;
+        System.out.println("Result of difference numbers " + num.a + " and " + num.b + ": " + diff);
         //Multiplication
-        int multi = a * b;
-        System.out.println("Result of multiplication numbers " + a + " and " + b + ": " + multi);
+        int multi = num.a * num.b;
+        System.out.println("Result of multiplication numbers " + num.a + " and " + num.b + ": " + multi);
         //Division
-        int div = a / b;
-        System.out.println("Result of division numbers " + a + " and " + b + ": " + div);
+        int div = num.a / num.b;
+        System.out.println("Result of division numbers " + num.a + " and " + num.b + ": " + div);
     }
 }
