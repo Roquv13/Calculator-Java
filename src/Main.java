1183e3675329996eb76c899a617d06702a737aff
Cezary Olszewski <czarekolszewski99@gmail.com>
2023-09-15 12:45:56 +0200

    Add new class BasicMath with methods add, diff, multi, div

--- src/Main.java
+++ src/Main.java
@@ -13,23 +13,39 @@ class Numbers {
         b = sc.nextInt();
     }
 }
+
+class BasicMath {
+    //Addition
+    public void add(int a, int b) {
+        int result = a + b;
+        System.out.println("Result of sum numbers " + a + " and " + b + ": " + result);
+    }
+    //Difference
+    public void diff(int a, int b) {
+        int result = a - b;
+        System.out.println("Result of difference numbers " + a + " and " + b + ": " + result);
+    }
+    //Multiplication
+    public void multi(int a, int b) {
+        int result = a * b;
+        System.out.println("Result of multiplication numbers " + a + " and " + b + ": " + result);
+    }
+    //Division
+    public void div(int a, int b) {
+        int result = a / b;
+        System.out.println("Result of division numbers " + a + " and " + b + ": " + result);
+    }
+}
 public class Main {
     public static void main(String[] args) {
         Numbers num = new Numbers();
+        BasicMath math = new BasicMath();
         num.scanA();
         num.scanB();
 
-        //Addition
-        int sum = num.a + num.b;
-        System.out.println("Result of sum numbers " + num.a + " and " + num.b + ": " + sum);
-        //Difference
-        int diff = num.a - num.b;
-        System.out.println("Result of difference numbers " + num.a + " and " + num.b + ": " + diff);
-        //Multiplication
-        int multi = num.a * num.b;
-        System.out.println("Result of multiplication numbers " + num.a + " and " + num.b + ": " + multi);
-        //Division
-        int div = num.a / num.b;
-        System.out.println("Result of division numbers " + num.a + " and " + num.b + ": " + div);
+        math.add(num.a, num.b);
+        math.diff(num.a, num.b);
+        math.multi(num.a, num.b);
+        math.div(num.a, num.b);
     }
 }
